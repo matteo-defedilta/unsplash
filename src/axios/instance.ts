69ec9a22392857d7595c9b604f42aa1aeb927616
matteo-defedilta <matteo.defedilta@gmail.com>
2024-02-14 15:23:27 +0100

    test build

--- src/axios/instance.ts
+++ src/axios/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const API_KEY = import.meta.env.VITE_API_KEY;
-const API_KEY = process.env.REACT_APP_APIKey;
+const API_KEY = import.meta.env.VITE_API_KEY;
+//const API_KEY = process.env.VITE_API_KEY;
 
 export const unsplashApi = axios.create({
 	baseURL: 'https://api.unsplash.com/photos/random',
